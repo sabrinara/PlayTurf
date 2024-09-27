8f6ba3fe7b46ec91567a4abf03851f14d823d24f
TechBuilderTeam <saashid18@gmail.com>
2024-09-27 23:59:04 +0600

    facility dashboard component created

---
+++ src/pages/DashboardUser/DashboardUser.tsx
@@ -0,0 +1,11 @@
+
+
+const DashboardUser = () => {
+    return (
+        <div>
+            <h1>DashboardUser</h1>
+        </div>
+    );
+};
+
+export default DashboardUser;
---
+++ src/pages/Facility/Facility.tsx
@@ -0,0 +1,11 @@
+
+
+const Facility = () => {
+    return (
+        <div>
+            <h1>Facility</h1>
+        </div>
+    );
+};
+
+export default Facility;
