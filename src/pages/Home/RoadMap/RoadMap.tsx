a4dde9469ec950c4ecde336bade2712fb4bdccd5
TechBuilderTeam <saashid18@gmail.com>
2024-09-27 23:21:28 +0600

    roadmap done for home

--- src/pages/Home/RoadMap/RoadMap.tsx
+++ src/pages/Home/RoadMap/RoadMap.tsx
@@ -5,14 +5,14 @@ const RoadMap = () => {
  
 
   return (
-    <div className="home flex justify-center items-center pl-40">
+    <div className="home flex justify-center items-center px-10 md:px-0 text-center md:pl-40">
       <div className="process start ">
         <ul >
           <li className='one'>
             <div className="icon">
               <img src="images/flat-icon/1-01.png" alt="Process Icon" />
             </div>
-            <div className="media-body w-2/3">
+            <div className="media-body w-full md:w-2/3">
               <h4>Step 1</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sequi nihil soluta sed quis ullam eius nesciunt natus veritatis! Quos accusamus molestias unde dicta quod eaque praesentium labore, explicabo quam. 1</p>
             </div>
@@ -21,7 +21,7 @@ const RoadMap = () => {
             <div >
               <img src="images/flat-icon/5-01.png" alt="Process Icon" />
             </div>
-            <div className="media-body w-2/3">
+            <div className="media-body w-full md:w-2/3">
               <h4>Step 2</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sequi nihil soluta sed quis ullam eius nesciunt natus veritatis! Quos accusamus molestias unde dicta quod eaque praesentium labore, explicabo quam. 2</p>
             </div>
@@ -30,7 +30,7 @@ const RoadMap = () => {
             <div className="">
               <img src="images/flat-icon/6-01.png" alt="Process Icon" />
             </div>
-            <div className="media-body  w-2/3">
+            <div className="media-body  w-full md:w-2/3">
               <h4>Step 3</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sequi nihil soluta sed quis ullam eius nesciunt natus veritatis! Quos accusamus molestias unde dicta quod eaque praesentium labore, explicabo quam. 3</p>
             </div>
@@ -39,7 +39,7 @@ const RoadMap = () => {
             <div >
               <img src="images/flat-icon/8-01.png" alt="Process Icon" />
             </div>
-            <div className="media-body  w-2/3">
+            <div className="media-body  w-full md:w-2/3">
               <h4>Step 4</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sequi nihil soluta sed quis ullam eius nesciunt natus veritatis! Quos accusamus molestias unde dicta quod eaque praesentium labore, explicabo quam. 4</p>
             </div>
@@ -48,7 +48,7 @@ const RoadMap = () => {
             <div >
               <img src="./images/flat-icon/9-01.png" alt="Process Icon" />
             </div>
-            <div className="media-body  w-2/3">
+            <div className="media-body  w-full md:w-2/3">
               <h4 className='text-center'>Step 5</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sequi nihil soluta sed quis ullam eius nesciunt natus veritatis! Quos accusamus molestias unde dicta quod eaque praesentium labore, explicabo quam. 5</p>
             </div>
--- src/pages/shared/Navbar.tsx
+++ src/pages/shared/Navbar.tsx
@@ -42,7 +42,7 @@ export default function Navbar() {
   ];
 
   return (
-    <div className=" md:top-6 py-5 bg-[#102e46] md:mx-4 md:rounded-full md:px-10">
+    <div className="sticky md:top-6 py-5 bg-[#102e46] md:mx-4 md:rounded-full md:px-10">
       <div className="flex  items-center justify-between lg:p-0 gap-2">
         <Link to="/">
           <div className="flex items-center">
